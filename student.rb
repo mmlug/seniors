d8d0aae9bde5a93b792b62fa7a125b9f00609273
khaipi <khaipi@gmail.com>
2013-11-13 23:51:34 +0100

    add junior? method

--- student.rb
+++ student.rb
@@ -10,6 +10,10 @@ class Student
 	def senior?
 		grade == 12
 	end
+	
+	def junior?
+		grade == 11
+	end
 
 	def to_s
 		"#{last_name}, #{first_name}"
@@ -20,11 +24,22 @@ def seniors(students)
 	students.select { |student| student.senior? }
 end
 
+def juniors(students)
+	students.select {|student| student.junior? }
+end
+
+
 fred = Student.new("Fred", "James", 12)
 sarah = Student.new("Sarah", "Smith", 12)
 jack = Student.new("Jack", "Gong", 11)
 all_students = [fred, sarah, jack]
 
 seniors(all_students).each do |student|
+	puts "Seniors"
 	puts student
+end
+
+juniors(all_students).each do |student|
+	puts "Juniors"
+	puts  student
 end
